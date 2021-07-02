7c9ebe51c63a15368e44c74b1a90fdb32ac10f08
Phong Do <phong.do@vn.woowahan.com>
2021-07-02 20:48:02 +0700

    Fix css

--- admin/src/components/LeftMenuHeader/Wrapper.js
+++ admin/src/components/LeftMenuHeader/Wrapper.js
@@ -1,28 +1,33 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-import Logo from "../../../assets/images/logo-strapi.png";
+import Logo from "../../assets/images/logo-strapi.png";
 
 const Wrapper = styled.div`
-  background-color: #ffffff;
-  padding-left: 2rem;
-  height: ${(props) => props.theme.main.sizes.leftMenu.height};
+  background-color: #eeeeee;
+  height: ${(props) => props.theme.main.sizes.header.height};
+
   .leftMenuHeaderLink {
     &:hover {
       text-decoration: none;
     }
   }
+
   .projectName {
     display: block;
+    height: 100%;
     width: 100%;
-    height: ${(props) => props.theme.main.sizes.leftMenu.height};
+    text-align: center;
+    height: ${(props) => props.theme.main.sizes.header.height};
+    vertical-align: middle;
     font-size: 2rem;
     letter-spacing: 0.2rem;
     color: $white;
+
     background-image: url(${Logo});
     background-repeat: no-repeat;
-    background-position: left center;
-    background-size: auto 2.5rem;
+    background-position: center center;
+    background-size: auto ${(props) => props.theme.main.sizes.header.height};
   }
 `;
 
